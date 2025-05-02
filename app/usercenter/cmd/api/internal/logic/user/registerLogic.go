3decbc55f424b6eb6454cbada88b0905ccddbe61
Mingtao Li <mingtao@xjtu.edu.cn>
2025-05-02 16:56:02 +0800

    部分更新registerlogic

--- app/usercenter/cmd/api/internal/logic/user/registerLogic.go
+++ app/usercenter/cmd/api/internal/logic/user/registerLogic.go
@@ -2,6 +2,11 @@ package user
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/ai4energy/Ai4EEMS-go/app/usercenter/model"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 
 	"github.com/ai4energy/Ai4EEMS-go/app/usercenter/cmd/api/internal/svc"
 	"github.com/ai4energy/Ai4EEMS-go/app/usercenter/cmd/api/internal/types"
@@ -26,6 +31,37 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 
 func (l *RegisterLogic) Register(req *types.RegisterReq) (resp *types.RegisterResp, err error) {
 	// todo: add your logic here and delete this line
+	mobile := sql.NullString{
+		String: req.Mobile,
+		Valid:  true,
+	}
 
+	user, err := l.svcCtx.UsersModel.FindOneByMobile(
+		context.Background(),
+		mobile,
+	)
+	if err != nil && !errors.Is(err, sqlx.ErrNotFound) {
+		l.Errorf("查找用户时出错: %v", err)
+		return nil, err
+	}
+	if user != nil {
+		return nil, errors.New("手机号已被注册")
+	}
+	// 插入新用户到数据库
+	newUser := &model.Ai4energyUser{
+		Mobile:   mobile,
+		Password: req.Password,
+	}
+	userId, err := l.svcCtx.UsersModel.Insert(context.Background(), newUser)
+	if err != nil {
+		l.Errorf("插入用户时出错: %v", err)
+		return nil, err
+	}
+	// 获取插入后的用户ID
+	newUser.Id, err = userId.LastInsertId()
+	if err != nil {
+		l.Errorf("获取用户ID时出错: %v", err)
+		return nil, err
+	}
 	return
 }
