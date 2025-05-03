ba7e9417943d9b0f104b4ca0984bdb093f8fc3e7
Mingtao Li <mingtao@xjtu.edu.cn>
2025-05-03 10:48:04 +0800

    变更user表名

--- app/usercenter/model/README.md
+++ app/usercenter/model/README.md
@@ -4,5 +4,5 @@
 
 ```shell
 # 注意用户名、密码、数据库名、表名，还有服务器的ip
-goctl model pg datasource --url="postgres://ai4eems_user:ai4eems_password@127.0.0.1:5432/ai4eems_db?sslmode=disable" --table="ai4energy_user" --dir .
+goctl model pg datasource --url="postgres://ai4eems_user:ai4eems_password@127.0.0.1:5432/ai4eems_db?sslmode=disable" --table="user" --dir .
 ```
--- deploy/postgres/initdb/usercenter.sql
+++ deploy/postgres/initdb/usercenter.sql
@@ -1,8 +1,8 @@
 -- 删除现有表（如果存在）
-DROP TABLE IF EXISTS ai4energy_user;
+DROP TABLE IF EXISTS "user";
 
 -- 创建新表（使用蛇形命名和 TIMESTAMPTZ）
-CREATE TABLE ai4energy_user (
+CREATE TABLE "user" (
     id SERIAL PRIMARY KEY NOT NULL,
     login_name VARCHAR(255) NOT NULL,
     real_name VARCHAR(255),
@@ -21,20 +21,20 @@ CREATE TABLE ai4energy_user (
 );
 
 -- 添加注释
-COMMENT ON TABLE ai4energy_user IS '用户表';
-COMMENT ON COLUMN ai4energy_user.login_name IS '登录名';
-COMMENT ON COLUMN ai4energy_user.real_name IS '真实姓名';
-COMMENT ON COLUMN ai4energy_user.password IS '密码';
-COMMENT ON COLUMN ai4energy_user.mobile IS '手机号';
-COMMENT ON COLUMN ai4energy_user.email IS '邮箱';
-COMMENT ON COLUMN ai4energy_user.avatar IS '头像地址';
-COMMENT ON COLUMN ai4energy_user.status IS '状态 0:未启用 1:正常';
-COMMENT ON COLUMN ai4energy_user.created_time IS '创建时间';
-COMMENT ON COLUMN ai4energy_user.updated_time IS '更新时间';
-COMMENT ON COLUMN ai4energy_user.deleted_time IS '删除时间';
-COMMENT ON COLUMN ai4energy_user.remark IS '备注';
+COMMENT ON TABLE "user" IS '用户表';
+COMMENT ON COLUMN "user".login_name IS '登录名';
+COMMENT ON COLUMN "user".real_name IS '真实姓名';
+COMMENT ON COLUMN "user".password IS '密码';
+COMMENT ON COLUMN "user".mobile IS '手机号';
+COMMENT ON COLUMN "user".email IS '邮箱';
+COMMENT ON COLUMN "user".avatar IS '头像地址';
+COMMENT ON COLUMN "user".status IS '状态 0:未启用 1:正常';
+COMMENT ON COLUMN "user".created_time IS '创建时间';
+COMMENT ON COLUMN "user".updated_time IS '更新时间';
+COMMENT ON COLUMN "user".deleted_time IS '删除时间';
+COMMENT ON COLUMN "user".remark IS '备注';
 
--- 创建触发器（自动更新 updated_time）
+-- 创建触发器函数（自动更新 updated_time）
 CREATE OR REPLACE FUNCTION update_updated_time_column()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -43,17 +43,17 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_ai4energy_user_updated_time
-BEFORE UPDATE ON ai4energy_user
+CREATE TRIGGER update_user_updated_time 
+BEFORE UPDATE ON "user" -- 应用于 "user" 表
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_time_column();
 
 -- 插入初始数据
-INSERT INTO ai4energy_user (login_name, real_name, password, mobile, email, status, remark)
+INSERT INTO "user" (login_name, real_name, password, mobile, email, status, remark)
 VALUES (
   'mingtaoli',
   '李明涛',
-  '$2a$06$A2NXVmndfGG/yl6ktMl96uA24s4wIAL62rRiBu6CEfbQL7vGYuf52',
+  '$2a$06$A2NXVmndfGG/yl6ktMl96uA24s4wIAL62rRiBu6CEfbQL7vGYuf52', -- 请确保这是一个安全的哈希密码
   '17782560245',
   'mingtao@xjtu.edu.cn',
   1,
